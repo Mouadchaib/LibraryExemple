9fd0adfc9134ded1a8e3c7b221e1c597291a3288
mouad <mouad.chaib@frencht.co>
2019-07-24 16:25:41 +0100

    add all repo

---
+++ mylibrary/src/main/java/com/mcz/mc/mylibrary/LibraryInfo.java
@@ -0,0 +1,13 @@
+package com.mcz.mc.mylibrary;
+
+import android.util.Log;
+
+public class LibraryInfo {
+
+    public LibraryInfo() {
+    }
+
+    public static void version(){
+        Log.e("Version", "votre version est 0.1");
+    }
+}
